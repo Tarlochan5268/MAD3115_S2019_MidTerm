133c6c20ca7940354e313f06be8320bcba676d5d
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-11 19:37:12 -0400

    user info plist added email and password

---
+++ C0751142_MidTerm_MAD3115S2019/C0751142_MidTerm_MAD3115S2019/MyDataStore.swift
@@ -0,0 +1,9 @@
+//
+//  MyDataStore.swift
+//  C0751142_MidTerm_MAD3115S2019
+//
+//  Created by admin on 2019-07-11.
+//  Copyright © 2019 Tarlochan5268. All rights reserved.
+//
+
+import Foundation
